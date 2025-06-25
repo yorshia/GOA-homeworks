635199d653d502194371fb505d2b695180796f5c
yorshia <sofokirtadze3@gmail.com>
2025-06-25 20:00:53 +0400

    second commit

--- day 00/day 1/homework(1).py
+++
@@ -1,8 +0,0 @@
-print("giorgi yorshia")
-
-
-print(5)      #integer   int
-print("5")    #string #str
-
-print(5 + int("5")) 
-print("giorgi aris " + "5" +"wlis")
--- day 00/homework/turtle .py
+++ day 00/homework/turtle .py
@@ -1,36 +1,67 @@
-from os import read
 from turtle import *
 
-speed(3)
-width(7)
-color("red")
-begin_fill()
-forward(200) 
-left(90)
+speed(9)
+width(3)
+color ("red")
+begin_fill ()
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (70)
+end_fill ()
+color ("blue")
+begin_fill ()
+left (90)
+forward (120)
+right (90)
+forward (60)
+right (90)
+forward (120)
+end_fill ()
 
-forward(200)
-left(90)
 
-forward(200)
-left(90)
-
-forward(100)
-left(90)
-end_fill()
+penup()
+goto(200,200)
+pendown()
 color("black")
-right(70)
-forward(100)
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+
+end_fill()
+color("brown")
+begin_fill()
+penup()
+goto(17,17)
+pendown()
+left(120)
+forward(25)
+
+right(270)
+forward(65)
 left(90)
-format(100)
+forward(30)
 left(90)
-forward(100)
-right(60)
-forward(100)
-right(60)
-forward(100)
-right(60)
-forward(100)
-right(60)
-forward(100)
-right(60)
-forward(100)
+forward(65)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
