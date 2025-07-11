669d457445c24f6c7708a7c1b6564baad707afa8
yorshia <sofokirtadze3@gmail.com>
2025-07-11 18:54:04 +0400

    second commit

---
+++ level004/classwork/work.py
@@ -0,0 +1,17 @@
+name="giorgi"
+print(5 + "name")       #1
+
+
+#კომენტარი ისეთი კოდი რომელსაც კომპიუტერი არ კითხულობს და ის გამოიყენებეა ჩვენი კოდის განსამარტად
+
+
+number1=5
+num2=2
+print(number1 * num2)
+num3=10
+print(num3 * 10)                   #3
+
+
+name="giorgi"
+name2="korshia"
+print("Hello, my name is"+ name + "Hello, my name is"+ name2)
---
+++ level004/homework/homework.py
@@ -0,0 +1,11 @@
+ #Debugging - გვეხმარება ჩვენ რომ ვიპოვოთ ბაგები ჩვენს კოდში
+#კორდიაცია არის როცა ორის სტრინგი ერთმანეთან ერთდებიან
+num1=5
+num2=13
+num3=40
+num4=77
+num=4
+num5=2
+print(num1 + num2 + num3 + num4 +num5)
+print(num1 * num2 * num3 * num4 * num5)
+print(num1/num2/num3/num4/num5)
