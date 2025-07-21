a8302605361b43a3b476ac79531a6c582ba49be8
yorshia <sofokirtadze3@gmail.com>
2025-07-21 17:44:53 +0400

    second commit

---
+++ level-008/classwork/classwork.py
@@ -0,0 +1,10 @@
+sigane=(input("შეიყვანეთ მართკუთხედის სიგანე: "))
+sigrze=(input("შეიყვანეთ მართკუთხედის სიგრძე: "))
+perrimetri= 2 * (sigrze+ sigane)
+print(perrimetri)
+
+
+age = int(input("ჩაწერე შენი ასაკი: "))
+
+
+print(age + 10)
