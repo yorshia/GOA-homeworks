0bdd967e136023d4b91e7eeacd6501d4effbaac2
yorshia <sofokirtadze3@gmail.com>
2025-07-25 17:20:34 +0400

    second commit

---
+++ level-010/classwork/classwork.py
@@ -0,0 +1,15 @@
+# explicity- როცა შენ უნდა მიუთითო რა გამოვიდეს
+#implicity-როცა კომპიუტერი თვითონ ცვლის მონაცემს
+name="giorgi"
+age=int(15)
+print("My name is " + name + ". I'm " + str(age) + " years old")
+num= float(input("შეიყვანეთ თქვენი საყვარელი რიცხვი: "))
+jami=num**5
+num1=15
+if num1> 0:
+    print("რიცხვი დადებითია")
+    elif num1< 0:
+    print("რიცხვი უარყოფითია")
+    
+else:
+    print("შეყვანილი რიცხვი ნულია")
