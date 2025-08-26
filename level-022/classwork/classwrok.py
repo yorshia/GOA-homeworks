4ae62c931d741a6761de535ca83d4db2125bd052
yorshia <sofokirtadze3@gmail.com>
2025-08-26 11:39:05 +0400

    second commit

--- level-022/classwork/classwrok.py
+++ level-022/classwork/classwrok.py
@@ -6,17 +6,24 @@
 
 # for i in range(10, -1, -1):
 #     print(i)
-i = 0
-while i <= 100:
-    print(i)
-    i += 2
-
-    i = 10
-while i >= 0:
-    print(i)
-    i -= 1
 
 i = 0
 while i <= 10:
     print(i)
     i += 1
+print("end of while loop")
+      
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+print("end of while loop")
+      
+
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+print("end of while loop")
+
+
