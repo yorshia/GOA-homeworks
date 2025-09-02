16a74084e00b85ff7f668de8edd511f613b0138e
yorshia <sofokirtadze3@gmail.com>
2025-09-02 18:29:47 +0400

    second commit

---
+++ level-026/classwork/classwork.py
@@ -0,0 +1,10 @@
+
+names = ["ანა", "ნიკა", "მარი", "ლაშა", "თიკა"]
+print(names[1:4])
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers[::2])
+print(numbers[2:6])
+print(numbers[2:-2:3])
+print(numbers[::-1])
---
+++ level-026/homework/homework.py
@@ -0,0 +1,5 @@
+number=[1,2,3,4,5,6,7,8,9,10]
+print(number[4])
+print(number[6])
+print(number[8])
+print(number[::-2])
