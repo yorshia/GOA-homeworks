4886592d95badcdffa655cc21fc5d418af37499d
yorshia <sofokirtadze3@gmail.com>
2025-09-11 12:01:38 +0400

    second commit

---
+++ level-030/classwork/classwork.py
@@ -0,0 +1,14 @@
+text = "gorup77"
+print(text.upper()) 
+
+text = "giorgi yorshia"
+print(text.lower())  
+
+text = "nika best"
+print(text.capitalize()) 
+
+text = "77 jgufi best "
+print(text.title()) 
+
+text = "hello world"
+print(text.find("world"))
---
+++ level-030/homework/homework.py
@@ -0,0 +1,52 @@
+# ფუნქცია არის კოდის ნაწილი რომელსაც სახელი აქვს და აკეთებს კონკრეტულ საქმეს
+# არგუმენტი არის ის ინფორმაცია რასაც ფუნქციას ვაძლევ როცა ვიძახებ მას
+
+text = "group77"
+print(text.upper())
+
+text = "giorgi yorshia"
+print(text.upper())
+
+text = "tato tato"
+print(text.upper())
+
+text = "nika maswi"
+print(text.upper())
+
+text = "best next"
+print(text.upper())
+   
+
+
+text = "Hello World!"
+print(text.lower())  
+
+text = "Pytho!"
+print(text.lower())
+
+text = "1234"
+print(text.lower())
+
+text = "agar vici ra davwero"
+print(text.lower())
+
+text = "12345678910"
+print(text.lower())
+
+
+
+text = "hELLO world!"
+print(text.capitalize())
+
+text = "123number at start"
+print(text.capitalize())
+
+text = "mIXED case sentence"
+print(text.capitalize())
+
+text = "before"
+print(text.capitalize())
+
+text = "new line first"
+print(text.capitalize())
+
