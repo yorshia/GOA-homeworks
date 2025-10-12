30ac689faa8034370f2890f93769e2623e4ef74e
qweqwe1qq <qweqwe@gmail.com>
2025-10-12 17:48:14 +0400

    second commit

---
+++ level-042/classwork/classwork.py
@@ -0,0 +1,26 @@
+# ფუნქცია არის კოდის ბლოკი რომელსაც შეგვიძლია მივცეთ რამე (ძალა ანუ ფუნქცია) და შეასრულოს მოქმედება
+# ჩვენ მისი გამოყენება შეგვიძლია  ამ კოდის დახმარებით:    def group77():
+                                                    #         print(რასაც გინდა ჩაწერ აქ: )
+                                                    #     group77 () 
+def test():
+    print("grup77 best")
+ 
+test()
+test()
+test()
+
+def num1():
+    sul = 0
+    for i in range(5):
+        num2 = (input("შემოიყვანე შენი რიცხვი: "))
+        sul = num2 
+        num1 = sul /5
+        print("არითმეტიკული არის:....",num1)
+
+
+def sayhai(name):
+    print("გამარჯობა  ბატონო",name)
+
+name2 = "giorgiis tato abrazebs"
+
+sayhai(name2)
---
+++ level-042/homework/homework.py
@@ -0,0 +1,17 @@
+def add_numbers(a, b):
+    return a + b 
+
+def average(a, b, c):
+    return (a + b + c) / 3
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return 
+    else:
+        return 
+    
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
