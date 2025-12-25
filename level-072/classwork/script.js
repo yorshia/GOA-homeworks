29c464c5fa8d6933d62f157ea916bcb061cf3d8f
qweqwe1qq <qweqwe@gmail.com>
2025-12-25 20:51:18 +0400

    second commit

---
+++ level-072/classwork/script.js
@@ -0,0 +1,18 @@
+// consol ჩვენ ვიყენებთ იმისთვის რო გავიგოთ ჩვენი ნამუშევარი javaში როგორც პიტონშია  და log ით ის გამოგვაქ კონსილში
+console.log(25);
+
+console.log("გამარჯობა");
+
+console.log(true);
+
+console.log(123456789012345678901234567890n);
+e.log(5 + 3);
+console.log(18 % 3);
+console.log(10 % 4);
+console.log(44 - 15);
+console.log(8* 7);
+console.log(4 * 5);
+console.log(20 / 4);
+console.log(9 / 3);
+console.log(10 + 20);
+console.log(10 - 4);
